db11a4f99287de760d237ebdff92d14ca27aac2a
Victor Gomes <90157378+victorfgomees@users.noreply.github.com>
2023-05-29 12:31:01 -0300

    Update main.py
    Adicionando comentários e docstrings

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ from lexer.lexerT1Lexer import lexerT1Lexer
 from antlr4.error.ErrorListener import ErrorListener
 
 class LexerErrorListener(ErrorListener):
+    """
+    Classe personalizada que trata erros léxicos durante a análise.
+    Herda da classe ErrorListener do ANTLR.
+    """
     def __init__(self, outfile):
         super().__init__()
         self.outfile = outfile
+    
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        """
+        Método chamado quando ocorre um erro léxico.
+        
+        Parâmetros:
+            - recognizer: O reconhecedor do lexer.
+            - offendingSymbol: O símbolo que causou o erro.
+            - line: O número da linha onde o erro ocorreu.
+            - column: A coluna onde o erro ocorreu.
+            - msg: A mensagem de erro.
+            - e: A exceção relacionada ao erro.
+        """
         errText = recognizer._input.getText(recognizer._tokenStartCharIndex, recognizer._input.index)
         errText = recognizer.getErrorDisplay(errText)
         if(len(errText) <= 1):
@@ -21,6 +37,7 @@ class LexerErrorListener(ErrorListener):
 
 
 def main():
+    # Obtém o nome do arquivo de entrada e de saída a partir dos argumentos da linha de comando
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
@@ -29,6 +46,7 @@ def main():
     lexer = lexerT1Lexer(input, output)
     lexer.addErrorListener(LexerErrorListener(output))
 
+    # Loop para gerar e processar os tokens
     while (token := lexer.nextToken()).type is not Token.EOF:
         token_type = lexer.ruleNames[token.type -1]
         if (token_type not in str(['IDENT','CADEIA','NUM_INT','NUM_REAL'])):
