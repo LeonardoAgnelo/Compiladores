a6d5c4de5162b74dc6eaf0e2580e3fac7b79cc1d
JoaoGGoncalves <joaogg123@gmail.com>
2023-06-12 12:36:03 -0300

    init t2 commit

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import sys
 import logging
 from antlr4 import *
-from lexer.lexerT1Lexer import lexerT1Lexer
+from Parser.LALexer import LALexer
+from Parser.LAParser import LAParser
 from antlr4.error.ErrorListener import ErrorListener
 
 class LexerErrorListener(ErrorListener):
+    """
+    Classe personalizada que trata erros léxicos durante a análise.
+    Herda da classe ErrorListener do ANTLR.
+    """
     def __init__(self, outfile):
         super().__init__()
         self.outfile = outfile
+    
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        """
+        Método chamado quando ocorre um erro léxico.
+        
+        Parâmetros:
+            - recognizer: O reconhecedor do lexer.
+            - offendingSymbol: O símbolo que causou o erro.
+            - line: O número da linha onde o erro ocorreu.
+            - column: A coluna onde o erro ocorreu.
+            - msg: A mensagem de erro.
+            - e: A exceção relacionada ao erro.
+        """
         errText = recognizer._input.getText(recognizer._tokenStartCharIndex, recognizer._input.index)
         errText = recognizer.getErrorDisplay(errText)
-        print(len(errText))
         if(len(errText) <= 1):
             self.outfile.write("Linha " + str(line) + ": " + errText + " - simbolo nao identificado\n")
         elif('{' in errText or '}' in errText):
             self.outfile.write("Linha " + str(line) + ": comentario nao fechado\n")
         elif('"' in errText):
             self.outfile.write("Linha " + str(line) + ": cadeia literal nao fechada\n")
+        raise Exception()
+    
+    
+class ParserErrorListener(ErrorListener):
+    def __init__(self, outfile):
+        super().__init__()
+        self.outfile = outfile
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.outfile.write("Erro em algum lugar")
         raise Exception()
 
 
@@ -27,15 +52,13 @@ def main():
 
     input = FileStream(input_file, encoding='utf-8')
     output = open(output_file, 'w')
-    lexer = lexerT1Lexer(input, output)
+    lexer = LALexer(input, output)
+    tokens = CommonTokenStream(lexer)
+    parser = LAParser(tokens, output)
     lexer.addErrorListener(LexerErrorListener(output))
-
-    while (token := lexer.nextToken()).type is not Token.EOF:
-        token_type = lexer.ruleNames[token.type -1]
-        if (token_type not in str(['IDENT','CADEIA','NUM_INT','NUM_REAL'])):
-            token_type = f"'{token.text}'"
-        
-        output.write(f"<'{token.text}',{token_type}>\n")
+    parser.addErrorListener(ParserErrorListener(output))
+    
+    parser.programa()   
 
 if __name__ == "__main__":
     try:
